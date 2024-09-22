f3f75982174b55cd3fa024110b5402d675214716
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-09-22 14:28:05 +0200

    empty commit

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username = "";
+  password = "";
 
+  onLogin() {
+
+  }
 }
