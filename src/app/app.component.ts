f9013237678489348d9bb53a27e3873a8b9a9369
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-09-22 13:17:22 +0200

    feat: add Home, Login, and Navigation components with basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from "./components/auth/login/login.component";
+import { HomeComponent } from "./components/home/home.component";
+import { NavigationComponent } from "./components/shared/navigation/navigation.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HomeComponent, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent }
+];
