3933215f83f6745e48afb4db9b5b78d23edd73bc
Velizar Ganchev <velizar_ganchev@hotmail.com>
2024-11-02 08:38:54 +0100

    feat: add Contacts and Summary components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { SignupComponent } from './components/auth/signup/signup.component';
 import { AddTaskComponent } from './components/task/add-task/add-task.component';
 import { BoardComponent } from './components/board/board.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { SummeryComponent } from './components/summery/summery.component';
 
 export const routes: Routes = [
     {
@@ -15,8 +17,10 @@ export const routes: Routes = [
         path: '',
         component: HomeComponent,
         children: [
+            { path: 'summery', component: SummeryComponent },
             { path: 'board', component: BoardComponent },
-            { path: 'add-task', component: AddTaskComponent }
+            { path: 'add-task', component: AddTaskComponent },
+            { path: 'contacts', component: ContactsComponent },
         ]
     },
     { path: 'login', component: LoginComponent },
---
+++ src/app/components/summery/summery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-summery',
+  standalone: true,
+  imports: [],
+  templateUrl: './summery.component.html',
+  styleUrl: './summery.component.scss'
+})
+export class SummeryComponent {
+
+}
